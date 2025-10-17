e37c8e31c136452a3ac1f0c7e933d3e43d8fec0d
Parrot Live user <user@localhost.localdomain>
2025-10-17 14:33:31 -0400

    Prueba6

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const swaggerOptions = {
         },
         servers: [
             {
-                url: 'http://localhost:3000',
+                url: 'https://p3-31899312.onrender.com',
             },
         ],
     },
